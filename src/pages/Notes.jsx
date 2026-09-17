7406603f4a0002211ab9d2fb759ee8bcebde582f
amzazibssh <edd.aitsaad@gmail.com>
2026-09-17 16:50:53 +0100

    comp: grid paper

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,3 +1,4 @@
+import { Grid, Paper } from '@mui/material';
 import React, { useEffect, useState } from 'react'
 
 export default function Notes() {
@@ -11,9 +12,28 @@ export default function Notes() {
 
   return (
     <div>
-      {notes.map(note => (
-        <p key={note.id}>{ note.title }</p>
-      ))}
+      <Grid container spacing={2}>
+        <Grid size={{md:3, xs:12}}>
+          <Paper>1</Paper> 
+        </Grid>
+        <Grid size={{md:3, xs:12}}>
+          <Paper>2</Paper>
+        </Grid>
+        <Grid size={{md:3, xs:12}}>
+          <Paper>3</Paper> 
+        </Grid>
+        <Grid size={{md:3, xs:12}}>
+          <Paper>4</Paper>
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={2}>
+        {notes.map( note => (
+          <Grid key={note.id} size={{md:3, xs:12}}>
+            <Paper>{note.title}</Paper>
+          </Grid>
+        ))}
+      </Grid>
     </div>
   )
 }
