1a804d927abf2af25d9a776b5730c450ae41fabd
amzazibssh <edd.aitsaad@gmail.com>
2026-09-17 14:57:34 +0100

    comp: textfield

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import Container from '@mui/material/Container'
 import AcUnitIcon from '@mui/icons-material/AcUnit';
 import SendIcon from '@mui/icons-material/Send';
 import { makeStyles } from '@mui/styles';
+import { TextField } from '@mui/material';
 
 
 const useStyle = makeStyles({
@@ -22,8 +23,18 @@ const useStyle = makeStyles({
 export default function Create() {
 
   const classes = useStyle()
-
-
+  const [title, setTitle] = useState("")
+  const [titleError, setTitleError] = useState(false)
+  const handlSubmit = (e) => {
+    e.preventDefault()
+    setTitleError(false)
+    if (title == ''){
+      setTitleError(true)
+    }
+    if(title){
+      console.log(title)
+    }
+  }
   return (
     <Container>
       <Typography
@@ -35,7 +46,17 @@ export default function Create() {
         Create page
       </Typography>
 
-      <Button
+      <form noValidate autoComplete='off' onSubmit={handlSubmit}>
+        <TextField
+          onChange={(e) => setTitle(e.target.value)}
+          label="title"
+          variant='filled'
+          fullWidth
+          color="secondary"
+          required
+          error={titleError}
+        />
+        <Button
         className={classes.btn}
         onClick={() => console.log('Button clicked')}
         type="submit"
@@ -44,6 +65,8 @@ export default function Create() {
       >
         submit
       </Button>
+      </form>
+      
       <br/>
 
       <AcUnitIcon color="error" fontSize="large"/>
