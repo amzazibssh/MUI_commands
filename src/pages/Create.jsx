0e3569f266082e59b370c467a6299b1529e75540
amzazibssh <edd.aitsaad@gmail.com>
2026-09-17 12:00:23 +0100

    comp:typography

---
+++ src/pages/Create.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Typography from '@mui/material/Typography'
+export default function Create() {
+  return (
+    <div>
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        noWrap
+      >
+        Create page
+      </Typography>
+    </div>
+  )
+}
